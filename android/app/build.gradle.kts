54837a1d295eee7be178b2568aee9403c4eecde5
Jenny <ngjenny0329jjj@gmail.com>
2025-03-18 01:41:03 +0800

    fix problem

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,8 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.0.13004108"
 
+    //compileSdkVersion 34
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -20,16 +22,27 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
+    // defaultConfig {
+    //     // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+    //     applicationId = "com.example.petapp"
+    //     // You can update the following values to match your application needs.
+    //     // For more information, see: https://flutter.dev/to/review-gradle-config.
+    //     minSdk = 23
+    //     targetSdk = flutter.targetSdkVersion
+    //     minSdkVersion 21
+    //     targetSdkVersion 34
+    //     versionCode = flutter.versionCode
+    //     versionName = flutter.versionName
+    // }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.petapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+
 
     buildTypes {
         release {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'screens/login_screen.dart';
@@ -10,7 +11,9 @@ import 'package:logging/logging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // Initialize logging
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((record) {
